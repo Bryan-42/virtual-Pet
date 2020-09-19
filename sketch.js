c2553dfb130b73aaba763e9c7725ef4a3acae329
Bryan-42 <68830449+Bryan-42@users.noreply.github.com>
2020-09-19 19:39:12 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
-var dog,dogHappu; 
+var dog,dogImg,dogHappy; 
 var canvas;
 var database;
 var foodStock;
 var foodS;
+var Food;
+var x = 0;
 
 function preload(){
-  dog = loadImage("images/dogImg.png");
+  dogIMG = loadImage("images/dogImg.png");
   dogHappy = loadImage("images/dogImg1.png");
 }
 
@@ -20,22 +22,33 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(46, 139, 87);
+  dog = createSprite(430,500);
+  dog.addImage("images/dogImg.png",dogIMG);
+  dog.scale = 0.3
   if (keyWentDown(UP_ARROW)){
     writeStock(foodS);
-    dog.addImage(dogHappy);
+    dog.addImage("images/dogImg1.png",dogHappy);
   }
+  drawSprites();
+  textSize(20);
+  fill("black");
+  text("Note: Press UP_AROW Key To Feed Drago Milk!",200,100);
+  textSize(20);
+  fill("black");
+  text("Food Remaning: "+foodStock,350,200);
 }
 function readStock(data){
   foodStock = data.val();
 }
 function writeStock(x){
   if (x<0){
-    x = 0;
+    console.log(x);
   }
   else{
     x = x -1;
+    console.log(x);
   }
   database.ref('/').update({
     Food : x
-  })
+  });
 }
