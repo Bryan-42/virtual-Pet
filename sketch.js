c235fb39cd511a8ddbf9b04054c44ceefd614277
Bryan-42 <68830449+Bryan-42@users.noreply.github.com>
2020-09-20 08:37:16 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 var dog,dogImg,dogHappy; 
-var canvas;
 var database;
 var foodStock;
 var foodS;
 var Food;
-var x = 0;
 
 function preload(){
   dogIMG = loadImage("images/dogImg.png");
@@ -12,22 +10,29 @@ function preload(){
 }
 
 function setup() {
-  canvas = createCanvas(800, 700);	
+  createCanvas(800, 700);	
   database = firebase.database();
+
   foodStock = database.ref('Food');
   foodStock.on("value",readStock);
+
+  dog = createSprite(430,500);
+  dog.scale = 0.3;
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(46, 139, 87);
-  dog = createSprite(430,500);
-  dog.addImage("images/dogImg.png",dogIMG);
-  dog.scale = 0.3
-  if (keyWentDown(UP_ARROW)){
+
+  if (keyDown(UP_ARROW)){
+    dog.addImage(dogHappy);
+  }
+  else{
+    dog.addImage(dogIMG);
+  }
+  if(keyWentDown(UP_ARROW)){
     writeStock(foodS);
-    dog.addImage("images/dogImg1.png",dogHappy);
   }
   drawSprites();
   textSize(20);
@@ -35,17 +40,18 @@ function draw() {
   text("Note: Press UP_AROW Key To Feed Drago Milk!",200,100);
   textSize(20);
   fill("black");
-  text("Food Remaning: "+foodStock,350,200);
+  text("Food Remaning: "+foodS,350,200);
 }
 function readStock(data){
-  foodStock = data.val();
+  foodS = data.val();
 }
 function writeStock(x){
-  if (x<0){
+  if (x<=0){
+    x=0;
     console.log(x);
   }
   else{
-    x = x -1;
+    x = x-1;
     console.log(x);
   }
   database.ref('/').update({
